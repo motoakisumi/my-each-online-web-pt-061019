14cfa8b12b75837c16ea6961bd52cc730d2c931a
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-26 13:15:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,15 @@
 def my_each (collection)
   i = 0
-  
+  new_array = []
   while i < collection.length
     
     yield collection[i]
+    new_array << collection[i]
     i = i + 1
     
   end
+  
+  return new_array
+  
 end
 
